79e0333ccb7ea8e6f53ab160c5214c58fcefedde
Micaela T <micaelatrzcinski@gmail.com>
2022-11-20 12:25:26 -0500

    Adding AbstractState

--- Game Programing Tutorial/Assets/Scripts/AbstractState.cs
+++ Game Programing Tutorial/Assets/Scripts/AbstractState.cs
@@ -1,7 +1,15 @@
+using UnityEngine;
+
 namespace DefaultNamespace
 {
     public class AbstractState
     {
+        protected FiniteStateMachine machine;
+        protected Vector3 mousePosition = Input.mousePosition;
+
+        public virtual void Update()
+        {
+        }
         
     }
 }
