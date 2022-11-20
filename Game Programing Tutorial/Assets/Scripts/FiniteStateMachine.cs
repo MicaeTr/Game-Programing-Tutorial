86dca8a862ca6a4328538c3d6fac91c50d65562e
Micaela T <micaelatrzcinski@gmail.com>
2022-11-20 12:03:01 -0500

    Rider Coding Section

---
+++ Game Programing Tutorial/Assets/Scripts/FiniteStateMachine.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FiniteStateMachine : MonoBehaviour
+{
+    private AbstractState currentState;
+
+    [SerializeField]private Image background;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = new FarState();
+        currentState.Update();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentState.Update();
+    }
+
+    public void switchColor(Color color)
+    {
+        background.color = color;
+    }
+
+    public void switchState(AbstractState newState)
+    {
+        currentState = newState;
+    }
+}
